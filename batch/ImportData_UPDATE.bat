5accee94b0f6af581bc56f46f01f68307e54528e
greentech420 <118946642+greentech420@users.noreply.github.com>
2025-05-12 10:47:46 +0900

    Delete batch directory

--- batch/ImportData_UPDATE.bat
+++
@@ -1,5 +0,0 @@
-del C:\oncweb\ImportData\emr_*.txt
-python C:\KUJSON2CSV\KUJSON2CSV\concat_csv.py C:\\KUGCP2CSV\\ImportData\\labs\\emr_labs_*.txt C:\\KUGCP2CSV\\ImportData\\emr_labs.txt
-cd C:\KUGCP2CSV\ImportData
-copy emr_*.txt C:\oncweb\ImportData
-A
